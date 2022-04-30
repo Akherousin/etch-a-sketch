332610b1ed198f0d663d2a6aa3057bb859646753
Arthur Badin <akherousin94@gmail.com>
2022-04-30 19:38:51 +0600

    add whiten mechanics

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ gridCreator.addEventListener("click", createNewGrid);
 function createNewGrid(e) {
   container.innerHTML = "";
 
-  const height = 10;
-  const width = 10;
-  //   const height = +prompt("select the height of the grid");
-  //   const width = +prompt("select the width of the grid");
+  const height = +prompt("select the height of the grid");
+  const width = +prompt("select the width of the grid");
   const amountOfBoxes = height * width;
 
   for (let i = 0; i < amountOfBoxes; i++) {
@@ -18,6 +16,8 @@ function createNewGrid(e) {
     box.classList.add("box");
     box.style.height = `${90 / width}rem`;
     box.style.width = `${90 / width}rem`;
+    box.style.backgroundColor = `black`;
+    box.style.opacity = 1;
     container.appendChild(box);
   }
 
@@ -25,17 +25,30 @@ function createNewGrid(e) {
   container.style.gridTemplateRows = `repeat(${height}, fit-content(3rem))`;
 }
 
+container.addEventListener("mouseover", whitenColor);
 container.addEventListener("mouseover", changeColor);
 
 function randomColor() {
-  let red = Math.floor(Math.random() * 256);
-  let green = Math.floor(Math.random() * 256);
-  let blue = Math.floor(Math.random() * 256);
-  return `rgba(${red}, ${green}, ${blue})`;
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  return `${red}, ${green}, ${blue}`;
 }
 
 function changeColor(e) {
   if (!e.target.classList.contains("box")) return;
   let box = e.target;
-  box.style.backgroundColor = randomColor();
+
+  box.style.backgroundColor = `rgba(${randomColor()})`;
+}
+
+function whitenColor(e) {
+  if (!e.target.classList.contains("box")) return;
+  let box = e.target;
+  let opacity = box.style.opacity;
+
+  if (+opacity > 0) {
+    opacity = +opacity - 0.1;
+    box.style.opacity = opacity;
+  } else if (+opacity <= 0) return;
 }
