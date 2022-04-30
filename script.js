74fa66873bd6e7b8a00e0c4684328400f0bc1ac3
Arthur Badin <akherousin94@gmail.com>
2022-04-30 18:22:46 +0600

    add change box color to a random color

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ gridCreator.addEventListener("click", createNewGrid);
 function createNewGrid(e) {
   container.innerHTML = "";
 
-  const height = +prompt("select the height of the grid");
-  const width = +prompt("select the width of the grid");
+  const height = 10;
+  const width = 10;
+  //   const height = +prompt("select the height of the grid");
+  //   const width = +prompt("select the width of the grid");
   const amountOfBoxes = height * width;
 
   for (let i = 0; i < amountOfBoxes; i++) {
@@ -21,4 +23,19 @@ function createNewGrid(e) {
 
   container.style.gridTemplateColumns = `repeat(${width}, fit-content(3rem))`;
   container.style.gridTemplateRows = `repeat(${height}, fit-content(3rem))`;
+}
+
+container.addEventListener("mouseover", changeColor);
+
+function randomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return `rgba(${red}, ${green}, ${blue})`;
+}
+
+function changeColor(e) {
+  if (!e.target.classList.contains("box")) return;
+  let box = e.target;
+  box.style.backgroundColor = randomColor();
 }
