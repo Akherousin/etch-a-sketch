e451baccf87232067d9c5e971667e65b8e746169
Arthur Badin <akherousin94@gmail.com>
2022-04-30 17:45:13 +0600

    add custom size selector

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i <= 15; i++) {
-  for (let j = 0; j <= 15; j++) {
+const gridCreator = document.querySelector(".btn-grid-creator");
+
+gridCreator.addEventListener("click", createNewGrid);
+
+function createNewGrid(e) {
+  container.innerHTML = "";
+
+  const height = +prompt("select the height of the grid");
+  const width = +prompt("select the width of the grid");
+
+  for (let i = 0; i <= height * width - 1; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
     container.appendChild(box);
   }
+
+  container.style.gridTemplateColumns = `repeat(${width}, fit-content(3rem))`;
+  container.style.gridTemplateRows = `repeat(${height}, fit-content(3rem))`;
 }
